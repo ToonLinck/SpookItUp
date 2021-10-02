173e267e022521465c3d892ea30c0bd8ece060e7
ToonLinck <Johanlinck96@gmail.com>
2021-10-02 15:25:57 +0200

    Added Box

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rig;
+    BoxCollider2D coll;
+
+    void Start()
+    {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+        coll = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
